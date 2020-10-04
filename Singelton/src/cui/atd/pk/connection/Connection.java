1aedc295b4e34566112ab1179e31bdedb535da5a
Shouzeb <shouzebhasan99@gmail.com>
2020-10-04 13:10:57 +0500

    adding files

---
+++ Singelton/src/cui/atd/pk/connection/Connection.java
@@ -0,0 +1,19 @@
+package cui.atd.pk.connection;
+
+public class Connection {
+	private static Connection instance;
+	
+	private Connection(){
+	}
+	
+	public static Connection getInstance(){
+		if(instance == null){
+			instance = new Connection();
+		}
+		return instance;
+	}
+	public void getData(){
+		System.out.println("This is the data retrieved from the DB");
+	}
+
+}
---
+++ Singelton/src/cui/atd/pk/connection/Demo.java
@@ -0,0 +1,11 @@
+package cui.atd.pk.connection;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Connection c = Connection.getInstance();
+		c.getData();
+
+	}
+
+}
