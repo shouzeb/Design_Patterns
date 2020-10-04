7c1935b3ed9112ea0e15bd5d010ae7a9e52e6007
Shouzeb <shouzebhasan99@gmail.com>
2020-10-04 13:16:31 +0500

    adding files

---
+++ Singelton_File_Reading/src/singeltonFileReading/Demo.java
@@ -0,0 +1,13 @@
+package singeltonFileReading;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Configuration config = Configuration.getInstance();
+		System.out.println(config.getValue("mode"));
+		System.out.println(config.getValue("font-size"));
+		System.out.println(config.getValue("font-type"));
+
+	}
+
+}
