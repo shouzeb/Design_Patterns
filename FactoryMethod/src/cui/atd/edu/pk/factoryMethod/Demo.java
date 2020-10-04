1dd4ff18c591d5f8a66a8b037c5c8bfc6e8f91cf
Shouzeb <shouzebhasan99@gmail.com>
2020-10-04 23:18:59 +0500

    adding files

---
+++ FactoryMethod/src/cui/atd/edu/pk/factoryMethod/Demo.java
@@ -0,0 +1,22 @@
+package cui.atd.edu.pk.factoryMethod;
+import java.util.ArrayList;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		ArrayList<Shape> shapes = new ArrayList<Shape>();
+		
+		CricleGeometry cricleGeomerty = new CricleGeometry();
+		SquareGeometry squareGeometry = new SquareGeometry();
+		
+		shapes.add(cricleGeomerty.createShape());
+		shapes.add(squareGeometry.createShape());
+		shapes.add(squareGeometry.createShape());
+		
+		for(Shape s:shapes){
+			s.draw();
+		}
+
+	}
+
+}
