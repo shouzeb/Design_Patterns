044a674a8dda0f8568b215de09a50544f0884b92
Shouzeb <shouzebhasan99@gmail.com>
2020-10-05 23:12:17 +0500

    adding files.

---
+++ PrototypeAccessControl/src/pk/edu/cui/atd/prototypeAccessControl/AccessControlProvider.java
@@ -0,0 +1,26 @@
+package pk.edu.cui.atd.prototypeAccessControl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AccessControlProvider {
+
+	private static Map<String, AccessControl>map = new HashMap<String, AccessControl>();
+	 
+    static {
+ 
+        map.put("user", new AccessControl("userr","do_work"));
+        map.put("admin", new AccessControl("admin","add and remove user"));
+        map.put("manager", new AccessControl("manager","generate/read reports"));
+        map.put("vp", new AccessControl("vp","modify reports"));
+    }
+ 
+    public static AccessControl getAccessControlObject(String controlLevel){
+        AccessControl ac = null;
+        ac = map.get(controlLevel);
+        if(ac!=null){
+            return ac.clone();
+        }
+        return null;
+    }
+}
