1b3179089b9de7479c12e0cb4813c8f3f7e527fd
Shouzeb <shouzebhasan99@gmail.com>
2020-10-06 14:24:20 +0500

    adding files

--- CarBuilder/src/pk/cui/carbuilder/FuelType.java
+++ CarBuilder/src/pk/cui/carbuilder/FuelType.java
@@ -10,5 +10,37 @@ public class FuelType{
 		this.combined = combined;
 		this.range = range;
 	}
+
+	public int getCity() {
+		return city;
+	}
+
+	public void setCity(int city) {
+		this.city = city;
+	}
+
+	public int getHighway() {
+		return highway;
+	}
+
+	public void setHighway(int highway) {
+		this.highway = highway;
+	}
+
+	public int getCombined() {
+		return combined;
+	}
+
+	public void setCombined(int combined) {
+		this.combined = combined;
+	}
+
+	public int getRange() {
+		return range;
+	}
+
+	public void setRange(int range) {
+		this.range = range;
+	}
 	
 }
