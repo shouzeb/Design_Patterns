6764d96c460f21f03c7dcbf5874ac53490ca409d
Shouzeb <shouzebhasan99@gmail.com>
2020-10-06 17:05:04 +0500

    updating files

--- CarBuilder/src/pk/cui/carbuilder/Demo.java
+++ CarBuilder/src/pk/cui/carbuilder/Demo.java
@@ -8,9 +8,12 @@ public class Demo {
 		// TODO Auto-generated method stub
 		Director director = new Director();
 		 CarBuilder builder = new CarBuilder();
-	     director.createSedanCar(builder);
-	     Car car = builder.getResult();
-	     System.out.println(car);
+	    director.createSedanCar(builder);
+	     Car car1 = builder.getResult();
+	     director.createSportsCar(builder);
+	     Car car2=builder.getResult();
+	     System.out.println(car1);
+	     System.out.println(car2);
 	}
 
 }
--- CarBuilder/src/pk/cui/carbuilder/Director.java
+++ CarBuilder/src/pk/cui/carbuilder/Director.java
@@ -1,13 +1,23 @@
 package pk.cui.carbuilder;
 
 public class Director {
-public void createSedanCar(Builder builder) {
 	
+public void createSedanCar(Builder builder) {
 	builder.setCarType(Type.Sedan);
 	builder.setBodyStyle(new BodyStyle(202.9, 76.2, 60.7, 112.9, 65.3, 65.5, 39.5));
 	builder.setBreakType(new BreakType("Four-wheel disc brakes: two ventilated. Electronic brake distribution"));
 	builder.setPowerType(new PowerType("285 hp @ 6,500 rpm; 253 ft lb of torque @ 4,000 rpm"));
 	builder.setFuelType(new FuelType(19, 29, 23, 437));
+	builder.setSeatType(new SeatType("Front seat center armrest.Rear seat center armrest.Split-folding rear seats"));
+	builder.setEngine(new Engine("3.5L Duramax V 6 DOHC"));
+
+}
+public void createSportsCar(Builder builder) {
+	builder.setCarType(Type.Sports);
+	builder.setBodyStyle(new BodyStyle(192.3, 75.5, 54.2, 112.3, 63.7, 64.1, 37.7));
+	builder.setBreakType(new BreakType("Four-wheel disc brakes: two ventilated. Electronic brake distribution"));
+	builder.setPowerType(new PowerType("285 hp @ 6,500 rpm; 253 ft lb of torque @ 4,000 rpm"));
+	builder.setFuelType(new FuelType(17, 28, 20, 380));
 	builder.setSeatType(new SeatType("Front seat center armrest.Rear seat center armrest.Split-folding rear seats"));
 	builder.setEngine(new Engine("3.5L Duramax V 6 DOHC"));
 
