12e01ddeaecf905b26853c934f1aec01c540f444
Shouzeb <shouzebhasan99@gmail.com>
2020-10-06 19:12:52 +0500

    adding files

---
+++ EspressoStoreBuilderPattern/src/pk/cui/espressoBuilder/CaffeFreddoBuilder.java
@@ -0,0 +1,45 @@
+package pk.cui.espressoBuilder;
+
+public class CaffeFreddoBuilder implements Builder{
+	private double exitTemperature;
+	private Type type;
+	private Roast roast;
+	private Size size;
+	private Length length;
+	@Override
+	public void setSize(Size size) {
+		this.size = size;
+		
+	}
+
+	@Override
+	public void setLength(Length length) {
+		this.length = length;
+		
+	}
+
+	@Override
+	public void setExitTemperature(double exitTemperature) {
+		this.exitTemperature = exitTemperature;
+		
+	}
+
+	@Override
+	public void setRoastLevel(Roast roast) {
+		this.roast = roast;
+		
+	}
+
+	@Override
+	public Espresso getResult() {
+	
+		return new Espresso(type, size, length, roast, exitTemperature);
+	}
+
+	@Override
+	public void setType(Type type) {
+		this.type = type;
+		
+	}
+	
+}
