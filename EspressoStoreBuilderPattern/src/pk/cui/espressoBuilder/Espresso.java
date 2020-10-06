d29586a637f5cae40f7b9a28287e6e8f43ef8725
Shouzeb <shouzebhasan99@gmail.com>
2020-10-06 19:13:15 +0500

    adding files

---
+++ EspressoStoreBuilderPattern/src/pk/cui/espressoBuilder/Espresso.java
@@ -0,0 +1,43 @@
+package pk.cui.espressoBuilder;
+
+public class Espresso {
+private double exitTemperature;
+private Type type;
+private Roast roast;
+private Size size;
+private Length length;
+public Espresso(Type type, Size size, Length length, Roast roast,double exitTemperature) {
+	super();
+	this.length = length;
+	this.type = type;
+	this.size = size;
+	this.roast = roast;
+	this.exitTemperature = exitTemperature;
+}
+
+public double getExitTemperature() {
+	return exitTemperature;
+}
+
+
+public Type getType() {
+	return type;
+}
+
+public Roast getRoast() {
+	return roast;
+}
+
+public Size getSize() {
+	return size;
+}
+
+public Length getLength() {
+	return length;
+}
+
+@Override
+public String toString() {
+	return "Espresso[size:"+getSize()+", length:"+getLength()+", roast:"+getRoast()+", temp:"+getExitTemperature()+"]";
+}
+}
---
+++ EspressoStoreBuilderPattern/src/pk/cui/espressoBuilder/StoreWorker.java
@@ -0,0 +1,28 @@
+package pk.cui.espressoBuilder;
+
+public class StoreWorker {
+private String name;
+
+public StoreWorker(String name) {
+	super();
+	this.name = name;
+}
+public Espresso  makeEspresso(Builder builder) {
+	builder.setSize(Size.Single);
+	builder.setLength(Length.Normale);
+	builder.setRoastLevel(Roast.Medium);
+	builder.setExitTemperature(70.0);
+	return builder.getResult();
+}
+public Espresso makeCaffeFreddo(Builder builder) {
+	builder.setSize(Size.Single);
+	builder.setLength(Length.Lungo);
+	builder.setRoastLevel(Roast.Ligth);
+	builder.setExitTemperature(-4.0);
+	return builder.getResult();
+}
+public String getName() {
+	return name;
+}
+
+}
