c5f0feb44674d1a21a0282aeeac7ad91bc934133
Shouzeb <shouzebhasan99@gmail.com>
2020-10-08 21:20:07 +0500

    adding files

---
+++ ObjectPool/src/cui/atd/objectPool/ResuableReturnedInstanceException.java
@@ -0,0 +1,11 @@
+package cui.atd.objectPool;
+
+public class ResuableReturnedInstanceException extends Exception {
+	
+		private static final long serialVersionUID = 1L;
+	public ResuableReturnedInstanceException() {
+
+		super("Reusble is returned");
+	}
+	}
+
