df29d5fc03802a2c1c756344c97cde774bedec70
Shouzeb <shouzebhasan99@gmail.com>
2020-10-08 21:20:13 +0500

    adding files

---
+++ ObjectPool/src/cui/atd/objectPool/DuplicateInstanceException.java
@@ -0,0 +1,9 @@
+package cui.atd.objectPool;
+
+public class DuplicateInstanceException extends Exception {
+	private static final long serialVersionUID = 1L;
+public DuplicateInstanceException() {
+	// TODO Auto-generated constructor
+	super("A duplicate");
+}
+}
