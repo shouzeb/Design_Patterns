3aec2a7d8c65d946060e752bd63997a1cc6b215c
Shouzeb <shouzebhasan99@gmail.com>
2020-10-09 08:44:31 +0500

    updating files

--- Multiton/src/pk/cui/atd/multitonCamera/demo.java
+++ Multiton/src/pk/cui/atd/multitonCamera/demo.java
@@ -1,5 +1,11 @@
 package pk.cui.atd.multitonCamera;
 
 public class demo {
-
+public static void main(String args[]) {
+	BackendServer server1 = BackendServer.getInstance("SErver1");
+	System.out.println(server1);
+	
+	BackendServer server2 = BackendServer.getInstance("server2");
+	System.out.println(server2);
+}
 }
