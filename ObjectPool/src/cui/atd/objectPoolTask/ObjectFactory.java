b6a99daf9cd5041e71d7b81a41af6f6b8e9b7927
Shouzeb <shouzebhasan99@gmail.com>
2020-10-09 22:55:01 +0500

    adding files

---
+++ ObjectPool/src/cui/atd/objectPoolTask/ObjectFactory.java
@@ -0,0 +1,10 @@
+package cui.atd.objectPoolTask;
+public interface ObjectFactory<T> {
+	   
+	   /**
+	    * Returns a new instance of an object type T.
+	    * 
+	    * @return T an new instance of the object type T
+	    */
+	   public abstract T createNew();
+	}
