219ea38f490c008c2c559e1c4b3ef64703f70381
Shouzeb <shouzebhasan99@gmail.com>
2020-10-09 22:55:14 +0500

    adding files

---
+++ AdapterXpayToPayD/src/task/PayD.java
@@ -0,0 +1,14 @@
+package task;
+
+public interface PayD {
+String getCustCardNo();
+String getCardOwnerName();
+String getCardExpMonthDate();
+int getCVVNo();
+double getTotalAmount();
+void setCustCardNo(String string);
+void  setCardOwnerName(String string);
+void  setCardExpMonthDate(String string);
+void  setCVVNo(int CVVNo);
+void  setTotalAmount(double amount);
+}
