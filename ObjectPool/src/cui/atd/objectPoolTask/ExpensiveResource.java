7a654985d8d41c56d9959c1b01c026eff144a1db
Shouzeb <shouzebhasan99@gmail.com>
2020-10-09 22:55:24 +0500

    adding files

---
+++ ObjectPool/src/cui/atd/objectPoolTask/ExpensiveResource.java
@@ -0,0 +1,13 @@
+package cui.atd.objectPoolTask;
+
+public class ExpensiveResource {
+	private String name;
+
+	public ExpensiveResource(String name) {
+		this.name = name;
+	}
+
+	public void doSomething() {
+		System.out.println("I am resource " + this.name + ".");
+	}
+}
