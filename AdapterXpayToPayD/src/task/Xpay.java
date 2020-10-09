caf3b4275d57b8e5ca4139440b8c5d5f2eece2a0
Shouzeb <shouzebhasan99@gmail.com>
2020-10-09 22:55:04 +0500

    adding files

---
+++ AdapterXpayToPayD/src/task/Xpay.java
@@ -0,0 +1,17 @@
+package task;
+
+public interface Xpay {
+String getCreditCardNo();
+String getCustomerName();
+String getCardExpMonth();
+String getCardExpYear();
+Short getCardCVVNo();
+double getAmount();
+
+void  setCreditCardNo(String string);
+void  setCustomerName(String string);
+void  setCardExpMonth(String string);
+void  setCardExpYear(String string);
+void  setCardCVVNo(Short cardCVVNo);
+void  setAmount(double amount);
+}
