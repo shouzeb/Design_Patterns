219e071a62c589873664901774a1458e4daae56a
Shouzeb <shouzebhasan99@gmail.com>
2020-10-14 23:33:00 +0500

    adding files

---
+++ ProxyPattern/src/cui/proxyPattern/Demo.java
@@ -0,0 +1,19 @@
+package cui.proxyPattern;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// use real service directly
+		//lazy initialization proxy
+Image image = new RealImage("test_10mb.jpg");
+System.out.println(image.getFileName());
+image.display();
+//use proxy
+Image proxyImage = new ProxyImage("test_10.jpg");
+	System.out.println(proxyImage.getFileName());
+	proxyImage.display();
+	
+	
+	}
+
+}
