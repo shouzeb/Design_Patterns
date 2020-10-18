c24ff6e6d7cb6d12bb4ebbdf5101a6e3f33c73f6
Shouzeb <shouzebhasan99@gmail.com>
2020-10-18 22:32:34 +0500

    uploading new files

---
+++ Facade/src/pk/cui/facade/AudioMixer.java
@@ -0,0 +1,9 @@
+package pk.cui.facade;
+import java.io.File;
+public class AudioMixer {
+    public File fix(VideoFile result){
+        System.out.println("AudioMixer: fixing audio...");
+        return new File("tmp");
+}
+    }
+
