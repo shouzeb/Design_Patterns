cf3c2f3ea219f5b1717b534b1cc795a18cdd3fee
Shouzeb <shouzebhasan99@gmail.com>
2020-10-19 22:06:36 +0500

    adding files

---
+++ BridgePatternLabTask/src/pk/cui/bridge/FrenchMealImp.java
@@ -0,0 +1,24 @@
+package pk.cui.bridge;
+public class FrenchMealImp implements MealImp {
+	public void getAppetizer()   { 
+		System.out.println("Appetizer: escargot"); 
+	}
+	public void getSoup()        {}
+	public void getSalad()       {}
+	public void getFish()        {}
+	public void getMeat()        { 
+		System.out.println("Meat:      coq au vin"); 
+	}
+	public void getPasta()       {}
+	public void getCheesePlate() {}
+	public void getBeverage()    { 
+		System.out.println("Beverage:  bordeaux");   
+	}
+	public void getDessert()     { 
+		System.out.println("Dessert:   creme brulee");    
+	}
+	public void getSorbet()      {}
+	public void getSandwich()    {}
+}
+
+
