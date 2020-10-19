8273e960c9e9e955a836da66203a9fd0d09f183c
Shouzeb <shouzebhasan99@gmail.com>
2020-10-19 22:06:39 +0500

    adding files

---
+++ BridgePatternLabTask/src/pk/cui/bridge/FiveCourseMeal.java
@@ -0,0 +1,22 @@
+package pk.cui.bridge;
+
+public class FiveCourseMeal extends Meal {
+	public FiveCourseMeal(String type) {
+		super(type);
+	}
+    public void getEnormousDinner() {
+        imp.getAppetizer();
+        imp.getSorbet();
+        imp.getSoup();
+        imp.getSorbet();
+        imp.getSalad();
+        imp.getSorbet();
+        imp.getFish();
+        imp.getSorbet();
+        imp.getMeat();
+        imp.getSorbet();
+        imp.getCheesePlate();
+        imp.getDessert();
+        imp.getBeverage();
+    }
+}
