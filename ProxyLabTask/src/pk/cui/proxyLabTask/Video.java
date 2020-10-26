008c6616332e17cfca17c68276156db8653e3812
Shouzeb <shouzebhasan99@gmail.com>
2020-10-26 23:21:52 +0500

    added files

---
+++ ProxyLabTask/src/pk/cui/proxyLabTask/Video.java
@@ -0,0 +1,12 @@
+package pk.cui.proxyLabTask;
+public class Video {
+	public String id;
+	public String title;
+	public String data;
+
+	Video(String id, String title) {
+		this.id = id;
+		this.title = title;
+		this.data = "Random video.";
+	}
+}
