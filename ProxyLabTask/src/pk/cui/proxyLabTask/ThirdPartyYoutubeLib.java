7500e721a286c01453f54dbf7a80e658e8829d23
Shouzeb <shouzebhasan99@gmail.com>
2020-10-26 23:22:05 +0500

    added files

---
+++ ProxyLabTask/src/pk/cui/proxyLabTask/ThirdPartyYoutubeLib.java
@@ -0,0 +1,9 @@
+package pk.cui.proxyLabTask;
+
+import java.util.HashMap;
+
+public interface ThirdPartyYoutubeLib {
+    HashMap<String, Video> popularVideos();
+
+    Video getVideo(String videoId);
+}
