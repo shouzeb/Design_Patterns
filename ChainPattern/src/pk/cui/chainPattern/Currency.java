04204a570e4aff709ae61017668103b55feb426d
Shouzeb <shouzebhasan99@gmail.com>
2020-10-31 23:01:27 +0500

    Files added.

---
+++ ChainPattern/src/pk/cui/chainPattern/Currency.java
@@ -0,0 +1,15 @@
+package pk.cui.chainPattern;
+
+public class Currency {
+int amount;
+
+public Currency(int amount) {
+	
+	this.amount = amount;
+}
+
+public int getAmount() {
+	return amount;
+}
+
+}
