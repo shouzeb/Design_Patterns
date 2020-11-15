f9c13ebbbd29f318d29bc85ea5dffb90e196fd7d
Shouzeb <shouzebhasan99@gmail.com>
2020-11-16 00:00:07 +0500

    added

---
+++ Assignment_two/src/pk/atd/cui/assignmentTwo/Demo.java
@@ -0,0 +1,8 @@
+package pk.atd.cui.assignmentTwo;
+
+public class Demo {
+	public static void main(String[] args) {
+		KitchenOperator.getInstance().init();
+		KitchenOperator.getInstance().finish();
+	}
+}
