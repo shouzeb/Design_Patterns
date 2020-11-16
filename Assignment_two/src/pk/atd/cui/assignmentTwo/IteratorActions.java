9a8126a674881e2959e5165377218983bb4d1408
Shouzeb <shouzebhasan99@gmail.com>
2020-11-16 22:36:55 +0500

    updating files

--- Assignment_two/src/pk/atd/cui/assignmentTwo/IteratorActions.java
+++ Assignment_two/src/pk/atd/cui/assignmentTwo/IteratorActions.java
@@ -3,19 +3,19 @@ package pk.atd.cui.assignmentTwo;
 import java.util.List;
 
 public class IteratorActions implements Iterator{
-	private List<KitchenApplianceReceiver> profiles;
+	private List<KitchenApplianceReceiver> appliance;
 	private int index=0;
 	
 	
 	public IteratorActions(List<KitchenApplianceReceiver> profiles) {
 		
-		this.profiles = profiles;
+		this.appliance = profiles;
 	}
 
 	@Override
 	public boolean hasNext() {
 		boolean result = false;
-		if(index < profiles.size())
+		if(index < appliance.size())
 			result = true;
 		return result;
 	}
@@ -24,7 +24,7 @@ public class IteratorActions implements Iterator{
 	public KitchenApplianceReceiver next() {
 		KitchenApplianceReceiver profile = null;
 		if(hasNext()){
-			profile = profiles.get(index);
+			profile = appliance.get(index);
 			index++;
 		}
 		return profile;
