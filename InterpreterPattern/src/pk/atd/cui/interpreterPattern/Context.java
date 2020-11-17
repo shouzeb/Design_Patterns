8109aeff0e8d6a9f70107b77688fb87786ecbf9d
Shouzeb <shouzebhasan99@gmail.com>
2020-11-17 23:05:37 +0500

    files added

---
+++ InterpreterPattern/src/pk/atd/cui/interpreterPattern/Context.java
@@ -0,0 +1,23 @@
+package pk.atd.cui.interpreterPattern;
+
+public class Context {
+String input;
+int output;
+
+public Context(String input) {
+	setInput(input);
+}
+public String getInput() {
+	return input;
+}
+public void setInput(String input) {
+	this.input = input;
+}
+public int getOutput() {
+	return output;
+}
+public void setOutput(int output) {
+	this.output = output;
+}
+
+}
