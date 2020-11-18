501c01229f6eac3d91e7053a9dca2e107cfe847e
Shouzeb <shouzebhasan99@gmail.com>
2020-11-18 22:16:04 +0500

    added files

---
+++ MediatorPattern/src/pk/atd/cui/mediatorPattern/ConcreteMediator.java
@@ -0,0 +1,26 @@
+package pk.atd.cui.mediatorPattern;
+
+public class ConcreteMediator implements Mediator{
+
+	private Component1 com1;
+	private Component2 com2;
+	
+	public ConcreteMediator(Component1 com1, Component2 com2) {
+		
+		this.com1 = com1;
+		com1.setMediator(this);
+		this.com2 = com2;
+		com2.setMediator(this);
+	}
+	
+	@Override
+	public void notify(Object sender, String event) {
+		if(event.equals("A")) {
+			System.out.println("Mediator reacts on A and trigers following operations:");
+		com2.doC();
+		}
+		
+		
+	}
+	
+}
