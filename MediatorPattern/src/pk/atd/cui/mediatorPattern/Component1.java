77033fa4bf997e64ce8090add3fab7aa3614b258
Shouzeb <shouzebhasan99@gmail.com>
2020-11-18 22:15:57 +0500

    added files

---
+++ MediatorPattern/src/pk/atd/cui/mediatorPattern/Component1.java
@@ -0,0 +1,13 @@
+package pk.atd.cui.mediatorPattern;
+
+public class Component1 extends BaseComponent{
+	public void doA() {
+		System.out.println("Component 1 does A.");
+		mediator.notify(this,"A");
+	}
+	public void doB() {
+		System.out.println("Component 1 does B.");
+		mediator.notify(this,"B");
+	}
+
+}
