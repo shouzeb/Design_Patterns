c40e0e314f13e2c6141739b51a5f8591dc5736b4
Shouzeb <shouzebhasan99@gmail.com>
2020-11-24 19:48:32 +0500

    added files.

---
+++ AssignmentThree/src/pk/cui/sc/mediator/hw/PartyMember.java
@@ -0,0 +1,9 @@
+package pk.cui.sc.mediator.hw;
+
+public interface PartyMember {
+	void joinedParty(Party party);
+	void partyAction(Action action);
+	void act(Action action);
+	int getHealth();
+	void setHealth(int health);
+}
