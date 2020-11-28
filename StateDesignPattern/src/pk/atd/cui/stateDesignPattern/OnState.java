552cb58e1efe3f003ef4865e9233e21ce9c9c3bd
Shouzeb <shouzebhasan99@gmail.com>
2020-11-28 23:34:12 +0500

    added file

---
+++ StateDesignPattern/src/pk/atd/cui/stateDesignPattern/OnState.java
@@ -0,0 +1,11 @@
+package pk.atd.cui.stateDesignPattern;
+
+public class OnState implements State{
+
+	@Override
+	public void doAction() {
+		// TODO Auto-generated method stub
+		System.out.println("TV is on.");
+	}
+
+}
