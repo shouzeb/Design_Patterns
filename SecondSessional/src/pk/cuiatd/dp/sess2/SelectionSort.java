091840a15bd96b10adcf052759f875c2962f34b0
Shouzeb <shouzebhasan99@gmail.com>
2020-12-02 19:01:10 +0500

    files uploaded

---
+++ SecondSessional/src/pk/cuiatd/dp/sess2/SelectionSort.java
@@ -0,0 +1,41 @@
+package pk.cuiatd.dp.sess2;
+
+//Java program for implementation of Selection Sort 
+class SelectionSort implements Sorting{ 
+	void sort(char[] arr) { 
+		int n = arr.length; 
+
+		// One by one move boundary of unsorted subarray 
+		for (int i = 0; i < n-1; i++) { 
+			// Find the minimum element in unsorted array 
+			int min_idx = i; 
+			for (int j = i+1; j < n; j++) 
+				if (arr[j] < arr[min_idx]) 
+					min_idx = j; 
+
+			// Swap the found minimum element with the first 
+			// element 
+			char temp = arr[min_idx]; 
+			arr[min_idx] = arr[i]; 
+			arr[i] = temp; 
+		} 
+	} 
+
+	// Prints the array 
+	void printArray(char arr[]) { 
+		int n = arr.length; 
+		for (int i=0; i<n; ++i) 
+			System.out.print(arr[i]+" "); 
+		System.out.println(); 
+	} 
+
+	// Driver code to test above 
+	public void SortMethod(char[] arrr) { 
+		SelectionSort ob = new SelectionSort(); 
+		char arr[] = arrr; 
+		ob.sort(arr); 
+		System.out.println("Sorted array"); 
+		ob.printArray(arr); 
+	} 
+} 
+
---
+++ SecondSessional/src/pk/cuiatd/dp/sess2/Sorting.java
@@ -0,0 +1,5 @@
+package pk.cuiatd.dp.sess2;
+
+public interface Sorting {
+	public void SortMethod(char[] arrr);
+}
