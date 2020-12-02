1771a335e2a030b90873624aeaf5ba3fd4625ca7
Shouzeb <shouzebhasan99@gmail.com>
2020-12-02 19:01:01 +0500

    files uploaded

---
+++ SecondSessional/src/pk/cuiatd/dp/sess2/Demo.java
@@ -0,0 +1,10 @@
+package pk.cuiatd.dp.sess2;
+
+public class Demo {
+public static void main(String args[]) {
+	char[] arr= {'f','v','a','d','t'};
+	CharacterClass characterClass = new CharacterClass();
+	characterClass.setS(new BubbleSort());
+	characterClass.sort(arr);
+}
+}
---
+++ SecondSessional/src/pk/cuiatd/dp/sess2/InsertionSort.java
@@ -0,0 +1,42 @@
+package pk.cuiatd.dp.sess2;
+
+//Java program for implementation of Insertion Sort 
+class InsertionSort implements Sorting{ 
+	/*Function to sort array using insertion sort*/
+	void sort(char arr[]) { 
+		int n = arr.length; 
+		for (int i = 1; i < n; ++i) { 
+			char key = arr[i]; 
+			int j = i - 1; 
+
+			/* Move elements of arr[0..i-1], that are 
+			greater than key, to one position ahead 
+			of their current position */
+			while (j >= 0 && arr[j] > key) { 
+				arr[j + 1] = arr[j]; 
+				j = j - 1; 
+			} 
+			arr[j + 1] = key; 
+		} 
+	} 
+
+	/* A utility function to print array of size n*/
+	static void printArray(char arr[]) { 
+		int n = arr.length; 
+		for (int i = 0; i < n; ++i) 
+			System.out.print(arr[i] + " "); 
+
+		System.out.println(); 
+	} 
+
+	// Driver method 
+	public void SortMethod(char[] arrr) { 
+		char arr[] = arrr; 
+
+		InsertionSort ob = new InsertionSort(); 
+		ob.sort(arr); 
+
+		printArray(arr); 
+	} 
+} 
+
