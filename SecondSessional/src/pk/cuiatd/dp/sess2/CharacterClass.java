77d7ae5fafc850a7db16e711c8c0de5adb4008f4
Shouzeb <shouzebhasan99@gmail.com>
2020-12-02 19:08:21 +0500

    uploading files

---
+++ SecondSessional/src/pk/cuiatd/dp/sess2/CharacterClass.java
@@ -0,0 +1,23 @@
+package pk.cuiatd.dp.sess2;
+
+public class CharacterClass {
+private String[] cha = new String[10]; 
+private Sorting s;
+
+public String[] getCha() {
+	return cha;
+}
+public void setCha(String[] cha) {
+	this.cha = cha;
+}
+public Sorting getS() {
+	return s;
+}
+public void setS(Sorting s) {
+	this.s = s;
+}
+
+void sort(char[] arr){
+	s.SortMethod(arr);
+}
+}
--- SecondSessional/src/pk/cuiatd/dp/sess2/Demo.java
+++ SecondSessional/src/pk/cuiatd/dp/sess2/Demo.java
@@ -2,7 +2,10 @@ package pk.cuiatd.dp.sess2;
 
 public class Demo {
 public static void main(String args[]) {
+	
 	char[] arr= {'f','v','a','d','t'};
+	System.out.println("Before sorting:");
+	System.out.println(arr);
 	CharacterClass characterClass = new CharacterClass();
 	characterClass.setS(new BubbleSort());
 	characterClass.sort(arr);
