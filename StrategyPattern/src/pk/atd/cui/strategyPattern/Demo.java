a685c134414056354e62fafcae0b453f79e00838
Shouzeb <shouzebhasan99@gmail.com>
2020-12-16 23:18:06 +0500

    updating files

--- StrategyPattern/src/pk/atd/cui/strategyPattern/Demo.java
+++ StrategyPattern/src/pk/atd/cui/strategyPattern/Demo.java
@@ -9,7 +9,7 @@ Item i1=new Item("code", 120);
 Item i2=new Item("coddsde", 1120);
 cart.addItem(i1);
 cart.addItem(i2);
-cart.setStrategy(new Paypal("myemail@sh.cm","asas"));
+cart.setStrategy (new Paypal("myemail@sh.cm","asas"));
 	cart.checkout();
 	
 	cart.setStrategy(new CreditCard("shouzeb", "cardNumber", "pin", "dateOfExpiry"));
