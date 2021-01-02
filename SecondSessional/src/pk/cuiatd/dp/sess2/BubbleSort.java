39ae66c33a562584517eb1a9b7874a04b295b0c6
Shouzeb <shouzebhasan99@gmail.com>
2021-01-02 22:40:07 +0500

    removed bugs

--- SecondSessional/src/pk/cuiatd/dp/sess2/BubbleSort.java
+++ SecondSessional/src/pk/cuiatd/dp/sess2/BubbleSort.java
@@ -6,7 +6,7 @@ class BubbleSort implements Sorting{
 		int n = arr.length; 
 		for (int i = 0; i < n-1; i++) 
 			for (int j = 0; j < n-i-1; j++) 
-				if (arr[j] > arr[j+1]) { 
+				 if (arr[j] > arr[j+1]) { 
 					// swap arr[j+1] and arr[j] 
 					char temp = arr[j]; 
 					arr[j] = arr[j+1]; 
