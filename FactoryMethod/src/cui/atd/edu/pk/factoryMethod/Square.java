362abe5b1f8592929263194fa52ff1e3326b0fc7
Shouzeb <shouzebhasan99@gmail.com>
2021-01-05 19:57:27 +0500

    removed bugs.

--- FactoryMethod/src/cui/atd/edu/pk/factoryMethod/Square.java
+++ FactoryMethod/src/cui/atd/edu/pk/factoryMethod/Square.java
@@ -1,9 +1,10 @@
 package cui.atd.edu.pk.factoryMethod;
 public class Square implements Shape{
 
+	
 	@Override
 	public void draw() {
-		System.out.println("square is drawn");
+		System.out.println("Square is drawn");
 		
 	}
 	
