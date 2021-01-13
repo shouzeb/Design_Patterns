a8be0a71620388630b2f3db04372e0c2e2102ba8
Shouzeb <shouzebhasan99@gmail.com>
2021-01-13 22:18:49 +0500

    uploading file.

---
+++ QuestionTwo/src/com/questionTwo/versionNumber/DocumentNotFoundException.java
@@ -0,0 +1,9 @@
+
+package com.questionTwo.versionNumber;
+
+
+public class DocumentNotFoundException extends Exception {
+  public DocumentNotFoundException(String message) {
+    super(message);
+  }
+}
