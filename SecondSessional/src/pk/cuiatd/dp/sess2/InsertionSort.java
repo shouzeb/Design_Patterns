97776a440ceea2df6e208fdfdfa61be0f3412656
Shouzeb <shouzebhasan99@gmail.com>
2021-01-16 23:11:02 +0500

    issue removed.

--- SecondSessional/src/pk/cuiatd/dp/sess2/InsertionSort.java
+++ SecondSessional/src/pk/cuiatd/dp/sess2/InsertionSort.java
@@ -5,6 +5,7 @@ class InsertionSort implements Sorting{
 	/*Function to sort array using insertion sort*/
 	void sort(char arr[]) { 
 		int n = arr.length; 
+		
 		for (int i = 1; i < n; ++i) { 
 			char key = arr[i]; 
 			int j = i - 1; 
