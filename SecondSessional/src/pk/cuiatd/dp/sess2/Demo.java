0d25c42e4ab9056e75c4905593e0274672815346
Shouzeb <shouzebhasan99@gmail.com>
2021-01-18 23:24:08 +0500

    removed bug

--- SecondSessional/src/pk/cuiatd/dp/sess2/Demo.java
+++ SecondSessional/src/pk/cuiatd/dp/sess2/Demo.java
@@ -3,7 +3,7 @@ package pk.cuiatd.dp.sess2;
 public class Demo {
 public static void main(String args[]) {
 	
-	char[] arr= {'f','v','a','d','t'};
+	char[] arr= {'f','v','a','d','t','c'};
 	
 	System.out.println("Before sorting:");
 	System.out.println(arr);
