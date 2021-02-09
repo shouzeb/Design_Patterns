282581e34c5b9d44d316998de80c94505dfe5194
Shouzeb <shouzebhasan99@gmail.com>
2021-02-09 23:02:33 +0500

    xrz

--- SecondSessional/src/pk/cuiatd/dp/sess2/Demo.java
+++ SecondSessional/src/pk/cuiatd/dp/sess2/Demo.java
@@ -3,7 +3,7 @@ package pk.cuiatd.dp.sess2;
 public class Demo {
 public static void main(String args[]) {
 	
-	char[] arr= {'f','v','a','d','t'};
+	char[] arr= {'f','v','a'};
 	
 	System.out.println("Before sorting:");
 	System.out.println(arr);
