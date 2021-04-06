8c2fbfea1635cf8517f372c2515b79b16e4fb3cd
Shouzeb <shouzebhasan99@gmail.com>
2021-04-06 23:29:22 +0500

    changed function

--- ObserverPattern2/src/pk/cui/observerPattern2/Demo.java
+++ ObserverPattern2/src/pk/cui/observerPattern2/Demo.java
@@ -1,6 +1,7 @@
 package pk.cui.observerPattern2;
 
 public class Demo {
+	
 	public static void main(String[] args) {
 Commentary commentary = new Commentary();
 Subscriber subscriber = new SMSSubscriber("Ahmed ALi [Abbottabad]");
