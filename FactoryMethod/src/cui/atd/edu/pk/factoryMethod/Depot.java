cc8a07f7b85db6246680e1e22b2cd88bc8b3764f
Shouzeb <shouzebhasan99@gmail.com>
2021-04-29 23:10:24 +0500

    changes

---
+++ FactoryMethod/src/cui/atd/edu/pk/factoryMethod/Depot.java
@@ -0,0 +1,30 @@
+package cui.atd.edu.pk.factoryMethod;
+
+public class Depot {
+private int productPrice,deliveryPrice,cashFlow;
+
+public int getProductPrice() {
+	return productPrice;
+}
+
+public void setProductPrice(int productPrice) {
+	this.productPrice = productPrice;
+}
+
+public int getDeliveryPrice() {
+	return deliveryPrice;
+}
+
+public void setDeliveryPrice(int deliveryPrice) {
+	this.deliveryPrice = deliveryPrice;
+}
+
+public int getCashFlow() {
+	return cashFlow;
+}
+
+public void setCashFlow(int cashFlow) {
+	this.cashFlow = cashFlow;
+}
+
+}
