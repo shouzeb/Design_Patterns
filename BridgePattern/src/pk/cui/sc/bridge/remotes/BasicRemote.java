bf3a77211cd882fd29c9a6c6f8d5dfdb8b8ead32
Shouzeb <shouzebhasan99@gmail.com>
2021-05-03 23:57:57 +0500

    added

--- BridgePattern/src/pk/cui/sc/bridge/remotes/BasicRemote.java
+++ BridgePattern/src/pk/cui/sc/bridge/remotes/BasicRemote.java
@@ -15,7 +15,7 @@ public class BasicRemote implements Remote {
 
 	@Override
 	public void togglePower() {
-		System.out.println("Remote: power toggle");
+		System.out.println("Remote: power toggle.");
 		if (device.isEnabled()) {
 			device.disable();
 		} else {
