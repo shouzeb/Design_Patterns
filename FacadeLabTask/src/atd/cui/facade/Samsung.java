9be7d1520d83afcc47dbe39b9dbbd26cd8e1b47e
Shouzeb <shouzebhasan99@gmail.com>
2021-06-02 23:38:25 +0500

    removed issue

--- FacadeLabTask/src/atd/cui/facade/Samsung.java
+++ FacadeLabTask/src/atd/cui/facade/Samsung.java
@@ -8,8 +8,12 @@ public class Samsung implements MobileShop {
 		System.out.println(" Samsung galaxy tab 3 ");  
 
 	}  
+	
 	@Override  
+	
 	public void price() {  
+	
 		System.out.println(" Rs 45000.00 ");  
+	
 	}  
 }  
